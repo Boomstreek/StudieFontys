bc2c6fb61d17b835884cd15c9403b4120ca13086
Bram Wieringa <jclwieringa@gmail.com>
2025-10-08 12:02:45 +0200

    07102025

---
+++ Sem01/Workshops/arduino/Scripts/MPU-6500/MPU-6500_001/MPU-6500_001.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+/*
+Documentatie:   https://www.arduinolearning.com/code/arduino-mpu6500-6-axis-motion-tracking-device.php
+                https://invensense.tdk.com/download-pdf/mpu-6500-datasheet/
+                https://github.com/LiquidCGS/FastIMU
+
+
+*/
